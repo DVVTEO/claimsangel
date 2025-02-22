9c7ecabb26b89518047b8e51b0f6a9c10c45010c
DVVTEO <chris@digitalvendingventures.com>
2025-02-22 18:05:22 +0000

    Add files via upload

---
+++ includes/Data/Countries.php
@@ -0,0 +1,423 @@
+<?php
+namespace ClaimsAngel\Data;
+
+/**
+ * Countries Class
+ * 
+ * Centralized management of country data including:
+ * - Country codes
+ * - Dialing codes
+ * - Flag URLs
+ * - Standardized country names
+ * 
+ * Created: 2025-02-21
+ * Last Modified: 2025-02-21 19:13:38
+ * Author: DVVTEO
+ */
+class Countries {
+    /**
+     * Instance of this class
+     *
+     * @var Countries
+     */
+    private static $instance = null;
+
+    /**
+     * Array of all supported countries with their data
+     *
+     * @var array
+     */
+    private $countries = [
+        'GB' => [
+            'name' => 'United Kingdom',
+            'dialing_code' => '44',
+            'flag_url' => 'gb.svg',
+            'enabled' => true
+        ],
+        'DE' => [
+            'name' => 'Germany',
+            'dialing_code' => '49',
+            'flag_url' => 'de.svg',
+            'enabled' => true
+        ],
+        'FR' => [
+            'name' => 'France',
+            'dialing_code' => '33',
+            'flag_url' => 'fr.svg',
+            'enabled' => true
+        ],
+        'ES' => [
+            'name' => 'Spain',
+            'dialing_code' => '34',
+            'flag_url' => 'es.svg',
+            'enabled' => true
+        ],
+        'IT' => [
+            'name' => 'Italy',
+            'dialing_code' => '39',
+            'flag_url' => 'it.svg',
+            'enabled' => true
+        ],
+        'NL' => [
+            'name' => 'Netherlands',
+            'dialing_code' => '31',
+            'flag_url' => 'nl.svg',
+            'enabled' => true
+        ],
+        'BE' => [
+            'name' => 'Belgium',
+            'dialing_code' => '32',
+            'flag_url' => 'be.svg',
+            'enabled' => true
+        ],
+        'PT' => [
+            'name' => 'Portugal',
+            'dialing_code' => '351',
+            'flag_url' => 'pt.svg',
+            'enabled' => true
+        ],
+        'IE' => [
+            'name' => 'Ireland',
+            'dialing_code' => '353',
+            'flag_url' => 'ie.svg',
+            'enabled' => true
+        ],
+        'DK' => [
+            'name' => 'Denmark',
+            'dialing_code' => '45',
+            'flag_url' => 'dk.svg',
+            'enabled' => true
+        ],
+        'SE' => [
+            'name' => 'Sweden',
+            'dialing_code' => '46',
+            'flag_url' => 'se.svg',
+            'enabled' => true
+        ],
+        'NO' => [
+            'name' => 'Norway',
+            'dialing_code' => '47',
+            'flag_url' => 'no.svg',
+            'enabled' => true
+        ],
+        'FI' => [
+            'name' => 'Finland',
+            'dialing_code' => '358',
+            'flag_url' => 'fi.svg',
+            'enabled' => true
+        ],
+        'AT' => [
+            'name' => 'Austria',
+            'dialing_code' => '43',
+            'flag_url' => 'at.svg',
+            'enabled' => true
+        ],
+        'CH' => [
+            'name' => 'Switzerland',
+            'dialing_code' => '41',
+            'flag_url' => 'ch.svg',
+            'enabled' => true
+        ],
+        'PL' => [
+            'name' => 'Poland',
+            'dialing_code' => '48',
+            'flag_url' => 'pl.svg',
+            'enabled' => true
+        ],
+        'CZ' => [
+            'name' => 'Czech Republic',
+            'dialing_code' => '420',
+            'flag_url' => 'cz.svg',
+            'enabled' => true
+        ],
+        'GR' => [
+            'name' => 'Greece',
+            'dialing_code' => '30',
+            'flag_url' => 'gr.svg',
+            'enabled' => true
+        ],
+        'HU' => [
+            'name' => 'Hungary',
+            'dialing_code' => '36',
+            'flag_url' => 'hu.svg',
+            'enabled' => true
+        ],
+        'RO' => [
+            'name' => 'Romania',
+            'dialing_code' => '40',
+            'flag_url' => 'ro.svg',
+            'enabled' => true
+        ]
+    ];
+
+    /**
+     * Constructor
+     */
+    private function __construct() {
+        // Initialize any necessary hooks or filters
+    }
+
+    /**
+     * Get instance of this class
+     *
+     * @return Countries
+     */
+    public static function get_instance() {
+        if (null === self::$instance) {
+            self::$instance = new self();
+        }
+        return self::$instance;
+    }
+
+    /**
+     * Get all countries
+     *
+     * @param bool $enabled_only Optional. Whether to return only enabled countries
+     * @return array Array of all countries
+     */
+    public function get_all($enabled_only = false) {
+        if (!$enabled_only) {
+            return $this->countries;
+        }
+
+        return array_filter($this->countries, function($country) {
+            return $country['enabled'];
+        });
+    }
+    
+    
+        /**
+     * Get all countries including their enabled status
+     * @return array Array of country data
+     */
+    public function get_all_countries() {
+        return $this->countries;
+    }
+
+    /**
+     * Get country data by ISO code
+     *
+     * @param string $code The ISO country code
+     * @return array|null Country data or null if not found
+     */
+    public function get_country($code) {
+        $code = strtoupper($code);
+        return isset($this->countries[$code]) ? $this->countries[$code] : null;
+    }
+
+    /**
+     * Find country ISO code by name
+     * 
+     * @param string $name The country name to search for
+     * @return string|null The ISO code if found, null if not found
+     */
+    public function find_country_code($name) {
+        // Normalize the input name
+        $search_name = strtolower(trim($name));
+
+        // Check if it's already a country code (case-insensitive)
+        $upper_code = strtoupper($search_name);
+        if (isset($this->countries[$upper_code])) {
+            return $upper_code;
+        }
+
+        // Direct match first
+        foreach ($this->countries as $code => $data) {
+            if (strtolower($data['name']) === $search_name) {
+                return $code;
+            }
+        }
+
+        // Try common variations
+$variations = [
+    // United Kingdom (GB)
+    'UK'                  => 'GB',
+    'United Kingdom'      => 'GB',
+    'Great Britain'       => 'GB',
+    'England'             => 'GB',
+
+    // Germany (DE)
+    'Germany'             => 'DE',
+    'Deutschland'         => 'DE',
+
+    // France (FR)
+    'France'              => 'FR',
+    'République française'=> 'FR',
+
+    // Spain (ES)
+    'Spain'               => 'ES',
+    'España'              => 'ES',
+    'Espana'              => 'ES',
+
+    // Italy (IT)
+    'Italy'               => 'IT',
+    'Italia'              => 'IT',
+
+    // Netherlands (NL)
+    'Netherlands'         => 'NL',
+    'Nederland'           => 'NL',
+    'Holland'             => 'NL',
+
+    // Belgium (BE)
+    'Belgium'             => 'BE',
+    'Belgique'            => 'BE',
+    'België'              => 'BE',
+
+    // Portugal (PT)
+    'Portugal'            => 'PT',
+    'República Portuguesa'=> 'PT',
+
+    // Ireland (IE)
+    'Ireland'             => 'IE',
+    'Éire'                => 'IE',
+
+    // Denmark (DK)
+    'Denmark'             => 'DK',
+    'Danmark'             => 'DK',
+
+    // Sweden (SE)
+    'Sweden'              => 'SE',
+    'Sverige'             => 'SE',
+
+    // Norway (NO)
+    'Norway'              => 'NO',
+    'Norge'               => 'NO',
+
+    // Finland (FI)
+    'Finland'             => 'FI',
+    'Suomi'               => 'FI',
+
+    // Austria (AT)
+    'Austria'             => 'AT',
+    'Österreich'          => 'AT',
+    'Osterreich'          => 'AT',
+
+    // Switzerland (CH)
+    'Switzerland'         => 'CH',
+    'Suisse'              => 'CH',
+    'Schweiz'             => 'CH',
+
+    // Poland (PL)
+    'Poland'              => 'PL',
+    'Polska'              => 'PL',
+
+    // Czech Republic (CZ)
+    'Czech Republic'      => 'CZ',
+    'Czechia'             => 'CZ',
+    'Ceská republika'     => 'CZ',
+    'Ceska republika'     => 'CZ',
+
+    // Greece (GR)
+    'Greece'              => 'GR',
+    'Hellas'              => 'GR',
+
+    // Hungary (HU)
+    'Hungary'             => 'HU',
+    'Magyarország'        => 'HU',
+
+    // Romania (RO)
+    'Romania'             => 'RO',
+    'România'             => 'RO'
+];
+
+        // Check variations
+        $search_key = array_search($search_name, array_map('strtolower', $variations));
+        if ($search_key !== false) {
+            return $variations[$search_key];
+        }
+
+        return null;
+    }
+
+    /**
+     * Get country name by ISO code
+     *
+     * @param string $code The ISO country code
+     * @return string|null Country name or null if not found
+     */
+    public function get_country_name($code) {
+        $country = $this->get_country($code);
+        return $country ? $country['name'] : null;
+    }
+
+    /**
+     * Get dialing code by ISO code
+     *
+     * @param string $code The ISO country code
+     * @return string|null Dialing code or null if not found
+     */
+    public function get_dialing_code($code) {
+        $country = $this->get_country($code);
+        return $country ? $country['dialing_code'] : null;
+    }
+
+    /**
+     * Get flag URL by ISO code
+     *
+     * @param string $code The ISO country code
+     * @return string|null Flag URL or null if not found
+     */
+    public function get_flag_url($code) {
+        $country = $this->get_country($code);
+        if (!$country) {
+            return null;
+        }
+
+        return plugins_url('assets/flags/' . $country['flag_url'], CLAIMS_ANGEL_PLUGIN_FILE);
+    }
+
+    /**
+     * Get countries as options array (for select fields)
+     *
+     * @param bool $enabled_only Optional. Whether to return only enabled countries
+     * @return array Array of countries in [code => name] format
+     */
+    public function get_countries($enabled_only = false) {
+        $countries = $this->get_all($enabled_only);
+        $options = [];
+
+        foreach ($countries as $code => $data) {
+            $options[$code] = $data['name'];
+        }
+
+        asort($options); // Sort by country name
+        return $options;
+    }
+
+    /**
+     * Format a phone number according to country format
+     *
+     * @param string $phone The phone number to format
+     * @param string $country_code The ISO country code
+     * @return string|null Formatted phone number or null if invalid
+     */
+    public function format_phone_number($phone, $country_code) {
+        $country = $this->get_country($country_code);
+        if (!$country) {
+            return null;
+        }
+
+        // Remove all non-numeric characters
+        $phone = preg_replace('/[^0-9]/', '', $phone);
+
+        // Remove country code if it exists
+        $dialing_code = $country['dialing_code'];
+        if (strpos($phone, $dialing_code) === 0) {
+            $phone = substr($phone, strlen($dialing_code));
+        }
+
+        // Add country code back without space
+        return '+' . $dialing_code . $phone;
+    }
+
+    /**
+     * Check if a country is enabled
+     *
+     * @param string $code The ISO country code
+     * @return bool Whether the country is enabled
+     */
+    public function is_country_enabled($code) {
+        $country = $this->get_country($code);
+        return $country ? $country['enabled'] : false;
+    }
+}
---
+++ includes/User/Manager.php
@@ -0,0 +1,130 @@
+<?php
+namespace ClaimsAngel\User;
+
+use ClaimsAngel\Data\Countries;
+
+/**
+ * User Manager Class
+ * Handles user interface modifications and data management
+ * 
+ * Created: 2025-02-21
+ * Last Modified: 2025-02-21 19:57:44
+ * Author: DVVTEO
+ */
+class Manager {
+    /**
+     * Initialize the user manager
+     */
+    public static function init() {
+        // Debug: Log initialization
+        error_log('ClaimsAngel User Manager Init Called');
+        
+        // Add country field to user forms
+        add_action('user_new_form', [__CLASS__, 'add_country_field_to_form']);
+        add_action('edit_user_profile', [__CLASS__, 'add_country_field_to_form']);
+        add_action('show_user_profile', [__CLASS__, 'add_country_field_to_form']);
+        
+        // Save country data
+        add_action('user_register', [__CLASS__, 'save_user_country']);
+        add_action('profile_update', [__CLASS__, 'save_user_country']);
+        
+        // Add validation for required country
+        add_filter('user_profile_update_errors', [__CLASS__, 'validate_user_country'], 10, 3);
+        
+        // Debug: Add action to check if we're on the right page
+        add_action('admin_init', function() {
+            global $pagenow;
+            if ($pagenow === 'user-new.php') {
+                error_log('ClaimsAngel: On user-new.php page');
+            }
+        });
+    }
+
+    /**
+     * Add country field to user forms
+     *
+     * @param \WP_User|null $user User object or null on creation
+     */
+    public static function add_country_field_to_form($user) {
+        // Debug: Log method call
+        error_log('ClaimsAngel add_country_field_to_form called');
+        
+        $countries = Countries::get_instance()->get_countries();
+        $selected = $user ? get_user_meta($user->ID, 'user_country', true) : '';
+        
+        // Debug: Log countries data
+        error_log('ClaimsAngel Countries Data: ' . print_r($countries, true));
+        
+        ?>
+        <table class="form-table">
+            <tr>
+                <th>
+                    <label for="user_country"><?php _e('Country', 'claims-angel'); ?></label>
+                </th>
+                <td>
+                    <select name="user_country" id="user_country" class="regular-text" required>
+                        <option value=""><?php _e('Select a country...', 'claims-angel'); ?></option>
+                        <?php foreach ($countries as $code => $name): ?>
+                            <option value="<?php echo esc_attr($code); ?>" <?php selected($selected, $code); ?>>
+                                <?php echo esc_html($name); ?>
+                            </option>
+                        <?php endforeach; ?>
+                    </select>
+                    <p class="description">
+                        <?php _e('Please select your country from the list.', 'claims-angel'); ?>
+                    </p>
+                </td>
+            </tr>
+        </table>
+        <?php
+        
+        // Debug: Log completion
+        error_log('ClaimsAngel add_country_field_to_form completed');
+    }
+
+    /**
+     * Save user country data
+     *
+     * @param int $user_id The user ID
+     */
+    public static function save_user_country($user_id) {
+        if (!current_user_can('edit_user', $user_id)) {
+            return;
+        }
+
+        $validator = new Validator();
+        $country = isset($_POST['user_country']) ? sanitize_text_field($_POST['user_country']) : '';
+        
+        if ($validator->is_valid_country($country)) {
+            update_user_meta($user_id, 'user_country', $country);
+        }
+    }
+
+    /**
+     * Validate user country on profile update
+     *
+     * @param \WP_Error $errors Error object
+     * @param bool      $update Whether this is an update
+     * @param \stdClass $user   User object
+     */
+    public static function validate_user_country($errors, $update, $user) {
+        $validator = new Validator();
+        $country = isset($_POST['user_country']) ? sanitize_text_field($_POST['user_country']) : '';
+        
+        if (!$validator->validate_country_selection($country)) {
+            $errors->add('user_country', __('Please select a valid country.', 'claims-angel'));
+        }
+
+        return $errors;
+    }
+
+    /**
+     * Get user's country
+     *
+     * @param int $user_id The user ID
+     * @return string Country code or empty string if not set
+     */
+    public static function get_user_country($user_id) {
+        return get_user_meta($user_id, 'user_country', true);
+    }
+}
